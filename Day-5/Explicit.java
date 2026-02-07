1ed0565998db17f39d904755e3c3a2b2d404481b
bannurikushal <bannurikushal59@gmail.com>
2026-02-07 19:18:55 +0530

    Day-5 files uploaded

---
+++ Day-5/Explicit.java
@@ -0,0 +1,12 @@
+public class Explicit {
+    static void main() {
+        long l=69365655;
+        int i=(int) l;
+        System.out.println(i);
+        float f=52.33f;
+        int a=(int) f;
+        System.out.println(a);
+        
+
+    }
+}
---
+++ Day-5/Implicit.java
@@ -0,0 +1,15 @@
+public class Implicit {
+    static void main() {
+        byte b=10;
+        int i=b;
+        System.out.println(i);
+        int a=567;
+        long l=a;
+        System.out.println(a);
+        float f=2.09f;
+        double d=f;
+        System.out.println(d);
+
+
+    }
+}
