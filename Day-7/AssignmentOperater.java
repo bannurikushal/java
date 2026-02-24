a82ab44564e4d2c72d0019b8f4c52426f3f54a01
bannurikushal <bannurikushal59@gmail.com>
2026-02-24 19:14:18 +0530

    Day-7 filesuploaded

---
+++ Day-7/AssignmentOperater.java
@@ -0,0 +1,15 @@
+public class AssignmentOperater {
+    static void main() {
+        int i=10;
+        i+=5;
+        System.out.println(i);
+        i-=50;
+        System.out.println(i);
+        i*=90;
+        System.out.println(i);
+        i/=2;
+        System.out.println(i);
+        i%=2;
+        System.out.println(i);
+    }
+}
