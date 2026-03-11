9b7910445cd07ddfc541eed819d6ffad982ec649
bannurikushal <bannurikushal59@gmail.com>
2026-03-11 18:56:07 +0530

    Day-18 files uploaded

---
+++ Day-18/ArmstronOfNum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ArmstronOfNum {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a num:");
+        int num = sc.nextInt();
+        int backup = num;
+        int result = 0;
+
+        while (num > 0) {
+            int remainder = num % 10;
+            result = result + (remainder * remainder * remainder);
+            num /= 10;
+        }
+
+        if (result == backup) {
+            System.out.println("It is a Armstrong");
+        } else {
+            System.out.println("It is not a Armstrong");
+        }
+    }
+}
+
+
