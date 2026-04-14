54426048b05a2f606776f3422b294386c3476750
bannurikushal <bannurikushal59@gmail.com>
2026-04-14 10:10:38 +0530

    Day-6 file uploade

---
+++ Day-26/BubbleSort.java
@@ -0,0 +1,35 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class BubbleSort {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of Array:");
+        int size= sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter the elements of Array:");
+        for (int i=0;i< arr.length-1;i++){
+            arr[i]= sc.nextInt();
+        }
+        BubbleSort(arr);
+        System.out.println("Reverse of array:");
+        for (int i=0;i< arr.length-1;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+        static void BubbleSort(int[] arr){
+        for (int i=0;i< arr.length-1;i++){
+            for (int j=0;i< arr.length-(i+1);j++){
+               if (arr[j]>arr[j+1]){
+                   int temp=arr[j];
+                   arr[j]=arr[j+1];
+                   arr[j+1]=temp;
+               }
+
+
+            }
+        }
+    }
+}
+
---
+++ Day-26/SelectionArray.java
@@ -0,0 +1,33 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class SelectionArray {
+    public static void main (String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the size Array:");
+        int size = sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter the elements of Array:");
+        for (int i=0;i< arr.length-1;i++){
+            arr[i]= sc.nextInt();
+        }
+        SortArray(arr);
+        for (int i=0;i< arr.length-1;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    static void SortArray(int [] arr){
+        for (int i=0;i< arr.length-1;i++){
+            int minIndex=i;
+            for (int j=i+1;j< arr.length;j++){
+                if (arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[minIndex];
+            arr[minIndex]=temp;
+        }
+    }
+}
