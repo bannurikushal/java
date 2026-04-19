3ac110f52b59cb18e0982a5b8e524cc4cfa525f6
bannurikushal <bannurikushal59@gmail.com>
2026-04-19 21:50:42 +0530

    Day-25 files uploaded

---
+++ Day-25/OddArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class OddArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int size = sc.nextInt();
+        int [] arr= new int[size];
+        System.out.println("Enter the elements of array:");
+        for (int i=0;i< arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+        for (int i=0;i< arr.length;i++){
+            if(arr[i]%2!=0){
+                System.out.println(arr[i]);
+            }
+            }
+    }
+}
