6ae49385ea4be3fb5da8bd31bab6fd1ef16cc453
bannurikushal <bannurikushal59@gmail.com>
2026-04-20 22:46:56 +0530

    Day-23 files uploaded

---
+++ Day-23/AssinOfArray.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class AssinOfArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array:");
+        int size = sc.nextInt();
+        int [] arr=new int[size];
+        System.out.println("Enter elements of Array:");
+        for (int i=0;i< arr.length;i++){
+            arr[i]= sc.nextInt();
+            System.out.println(arr[i]);
+        }
+    }
+}
